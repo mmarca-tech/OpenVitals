920f5f6f5b4e2e38818ef686f9be3c7b64ae0d76
Manuel <manuel@mmarca.tech>
2026-04-18 22:19:21 +0300

    Fix libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
 }
