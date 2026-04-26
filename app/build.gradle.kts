6602974f38344de36bc6fc2af80278332eb8f226
Marcatili, Manuel <Manuel.Marcatili@riverty.com>
2026-04-26 15:42:15 +0300

    chore: release 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "tech.mmarca.openvitals"
         minSdk = 26
         targetSdk = 36
-        versionCode = 113
-        versionName = "0.1.13"
+        versionCode = 200
+        versionName = "0.2.0"
     }
 
     signingConfigs {
